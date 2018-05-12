cc71d691a9da7badc84c398309d29e7afdde6b4f
Petro Tarnavskyy <petro.tarnavskyy@lnu.edu.ua>
2018-05-12 15:14:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,23 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-2.times 
-{
-  
-}
-
-
+  ind=0
+  2.times
+  {
+    2.times
+    {
+      print " #{board[i]} |"
+      ind++
+    }
+    puts " #{board[i]} "
+    puts "-----------"
+    ind++
+  }
+    2.times
+    {
+      print " #{board[i]} |"
+      ind++
+    }
+    puts " #{board[i]} "
+    ind++
 end
