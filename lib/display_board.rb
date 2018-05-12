a06c3e2a8508c66e2600686274fbe3c1065931c2
Petro Tarnavskyy <petro.tarnavskyy@lnu.edu.ua>
2018-05-12 15:17:28 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  ind=0
+  2.times{
+    2.times{
+      print " #{board[ind]} |"
+      ind=ind+1
+    }
+    puts " #{board[ind]} "
+    puts "-----------"
+    ind=ind+1
+  }
+    2.times{
+      print " #{board[ind]} |"
+      ind=ind+1
+    }
+    puts " #{board[ind]} "
+
+end
